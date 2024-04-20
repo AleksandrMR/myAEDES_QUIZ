111275bf46421eb4b2c214c6e062a87af2d638cf
Aleksandr Milashevski <aleksandr.milashevski.ios@gmail.com>
2024-04-20 10:43:56 +0200

    project created

---
+++ myAEDES_QUIZ/myAEDES_QUIZApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAEDES_QUIZApp.swift
+//  myAEDES_QUIZ
+//
+//  Created by Aleksandr Milashevski on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct myAEDES_QUIZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
